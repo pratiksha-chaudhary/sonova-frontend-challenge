b0f3ad5177b97712b0a32fc55668583d680b0427
pratiksha-chaudhary <pratiksha.chaudhary89@gmail.com>
2021-06-26 13:53:37 +0200

    final submission

--- src/components/Benefits/index.js
+++ src/components/Benefits/index.js
@@ -1,38 +1,38 @@
-import Benefit from '../Benefit';
-import Button from '../Button';
-import benefits from './benefits.json';
-import './index.scss';
+import Benefit from "../Benefit";
+import Button from "../Button";
+import benefits from "./benefits.json";
+import "./index.scss";
 
 function Benefits() {
-  return (
-    <div className="Benefits main-app-placement">
-      <div className="benefit-list">
-        {benefits.map((benefit) => (
-          <Benefit text={benefit} />
-        ))}
-      </div>
-      <div className="guarantee-container">
-        <div className="guarantee">
-          <img
-            src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/661e4500-geers-service-trust-symbol_106405m000000000000000.png"
-            alt="service guarantee"
-            loading="lazy"
-          />
-          <img
-            src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/6e35485c-geers-best-price-trust-symbol_106405m000000000000000.png"
-            alt="best price guarantee"
-            loading="lazy"
-          />
-          <img
-            src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/ad67a67d-geers-satisfaction-trust-symbol_106405m000000000000000.png"
-            alt="satisfaction guarantee"
-            loading="lazy"
-          />
+    return (
+        <div className="Benefits main-app-placement">
+            <div className="benefit-list">
+                {benefits.map((benefit, i) => (
+                    <Benefit text={benefit} key={i} />
+                ))}
+            </div>
+            <div className="guarantee-container">
+                <div className="guarantee">
+                    <img
+                        src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/661e4500-geers-service-trust-symbol_106405m000000000000000.png"
+                        alt="service guarantee"
+                        loading="lazy"
+                    />
+                    <img
+                        src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/6e35485c-geers-best-price-trust-symbol_106405m000000000000000.png"
+                        alt="best price guarantee"
+                        loading="lazy"
+                    />
+                    <img
+                        src="https://d9hhrg4mnvzow.cloudfront.net/info.geers.de/zdf/ad67a67d-geers-satisfaction-trust-symbol_106405m000000000000000.png"
+                        alt="satisfaction guarantee"
+                        loading="lazy"
+                    />
+                </div>
+                <Button text="massa enim nec dui" />
+            </div>
         </div>
-        <Button text="massa enim nec dui" />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Benefits;
--- src/components/SurveySection/index.js
+++ src/components/SurveySection/index.js
@@ -1,19 +1,27 @@
-import Survey from '../Survey';
-import surveys from './surveys.json';
-import './index.scss';
+import Survey from "../Survey";
+import surveys from "./surveys.json";
+import "./index.scss";
 
 function SurveySection() {
-  return (
-    <div className="SurveySection main-app-placement">
-      <h3 className="heading">sed pulvinar proin gravida hendrerit</h3>
-      <div className="container">
-        {surveys.map(({ percentUsers, description }) => (
-          <Survey percentUsers={percentUsers} text={description} />
-        ))}
-        <Survey percentUsers="" text="*Eurotrak 2018" className="asterisk-content"/>
-      </div>
-    </div>
-  );
+    return (
+        <div className="SurveySection main-app-placement">
+            <h3 className="heading">sed pulvinar proin gravida hendrerit</h3>
+            <div className="container">
+                {surveys.map(({ percentUsers, description }, i) => (
+                    <Survey
+                        percentUsers={percentUsers}
+                        text={description}
+                        key={i}
+                    />
+                ))}
+                <Survey
+                    percentUsers=""
+                    text="*Eurotrak 2018"
+                    className="asterisk-content"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default SurveySection;
